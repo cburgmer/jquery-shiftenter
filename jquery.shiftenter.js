27f935086135335df0cb209c78a249ca5ab983d6
Christoph Burgmer <christoph@nwebs.de>
2011-05-04 01:10:14 +0200

    One up version

--- jquery.shiftenter.js
+++ jquery.shiftenter.js
@@ -1,5 +1,5 @@
 /*
- * shiftenter: a jQuery plugin, version: 0.0.1 (2011-03-29)
+ * shiftenter: a jQuery plugin, version: 0.0.2 (2011-05-04)
  * tested on jQuery v1.5.0
  *
  * ShiftEnter is a jQuery plugin that makes it easy to allow submitting a form
